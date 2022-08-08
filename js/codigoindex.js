de1d947a8149af89f4ef3149a23acef24b111501
Jet-Talledo <jezrel_talledo2100@hotmail.com>
2022-08-08 16:19:27 -0500

    llanta

--- js/codigoindex.js
+++ js/codigoindex.js
@@ -48,3 +48,77 @@ function verprod4(){
 function noverprod4(){
     document.getElementById('verprod4').style.height = '0';
 }
+
+/*****cambia color de letra de categorias******/
+var galeria = document.getElementById('galerycate1');
+var nompro = document.getElementById('nompro1');
+var galeria2 = document.getElementById('galerycate2');
+var nompro2 = document.getElementById('nompro2');
+var galeria3 = document.getElementById('galerycate3');
+var nompro3 = document.getElementById('nompro3');
+var galeria4 = document.getElementById('galerycate4');
+var nompro4 = document.getElementById('nompro4');
+var galeria5 = document.getElementById('galerycate5');
+var nompro5 = document.getElementById('nompro5');
+var galeria6 = document.getElementById('galerycate6');
+var nompro6 = document.getElementById('nompro6');
+var galeria7 = document.getElementById('galerycate7');
+var nompro7 = document.getElementById('nompro7');
+
+galeria.onmouseover = aparece;
+galeria.onmouseout = sale;
+galeria2.onmouseover = aparece2;
+galeria2.onmouseout = sale2;
+galeria3.onmouseover = aparece3;
+galeria3.onmouseout = sale3;
+galeria4.onmouseover = aparece4;
+galeria4.onmouseout = sale4;
+galeria5.onmouseover = aparece5;
+galeria5.onmouseout = sale5;
+galeria6.onmouseover = aparece6;
+galeria6.onmouseout = sale6;
+galeria7.onmouseover = aparece7;
+galeria7.onmouseout = sale7;
+
+function aparece(){
+    nompro.style.color = "red";
+}
+function sale(){
+    nompro.style.color = "black";
+}
+function aparece2(){
+    nompro2.style.color = "red";
+}
+function sale2(){
+    nompro2.style.color = "black";
+}
+function aparece3(){
+    nompro3.style.color = "red";
+}
+function sale3(){
+    nompro3.style.color = "black";
+}
+function aparece4(){
+    nompro4.style.color = "red";
+}
+function sale4(){
+    nompro4.style.color = "black";
+}
+function aparece5(){
+    nompro5.style.color = "red";
+}
+function sale5(){
+    nompro5.style.color = "black";
+}
+function aparece6(){
+    nompro6.style.color = "red";
+}
+function sale6(){
+    nompro6.style.color = "black";
+}
+function aparece7(){
+    nompro7.style.color = "red";
+}
+function sale7(){
+    nompro7.style.color = "black";
+}
