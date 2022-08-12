aa30e9aa67c891cd4ab7a52709fd9d7cdb81f713
Jet-Talledo <jezrel_talledo2100@hotmail.com>
2022-08-11 23:22:10 -0500

    g

---
+++ js/parabuscar.js
@@ -0,0 +1,6 @@
+function mosbarbus(){
+    document.getElementById('parmosbarrbus').style.display = 'flex';
+}
+function cerbus(){
+    document.getElementById('parmosbarrbus').style.display = 'none';
+}
