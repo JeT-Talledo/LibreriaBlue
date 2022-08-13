6050f7ae5a69613771b329b62f52f708c2164524
Jet-Talledo <jezrel_talledo2100@hotmail.com>
2022-08-12 22:27:04 -0500

    ll

--- js/categorias.js
+++ js/categorias.js
@@ -4,3 +4,23 @@ function cerrfilt(){
 function mosfilt(){
     document.getElementById('contcate').style.width = '100%';
 }
+
+
+/***** para cambiar de pagina*******/
+function mostrarone(){
+    document.getElementById('paginaone').style.display = 'flex';
+    document.getElementById('paginados').style.display = 'none';
+    document.getElementById('pag1').style.backgroundColor = '#005EFF';
+    document.getElementById('pag1').style.color = 'white';
+    document.getElementById('pag2').style.backgroundColor = '#efe9e9';
+    document.getElementById('pag2').style.color = 'black';
+}
+
+function mostrardos(){
+    document.getElementById('paginaone').style.display = 'none';
+    document.getElementById('paginados').style.display = 'flex';
+    document.getElementById('pag1').style.backgroundColor = '#efe9e9';
+    document.getElementById('pag1').style.color = 'black';
+    document.getElementById('pag2').style.backgroundColor = '#005EFF';
+    document.getElementById('pag2').style.color = 'white';
+}
